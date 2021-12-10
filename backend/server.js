4828191db82a7727637600e541b38f3c40a144e0
Ivan <radisav80@yahoo.com>
2021-12-10 23:21:49 +0100

    changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -31,15 +31,29 @@ app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
-
 const __dirname = path.resolve()
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-app.get('/', (req, res) => {
+//path for production
+if(process.env.NODE_ENV === 'production') {
 
-    res.send('API is running...')
-})
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+
+}else {
+
+    app.get('/', (req, res) => {
+
+        res.send('API is running...')
+    })
+}
+
+
+
+
+
 
 
 app.use(notFound)
